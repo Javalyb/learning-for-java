070e4b2354d6795eb9162431eb89fac6762c703c
LYB <lyb0606@163.com>
2021-01-29 15:35:19 +0800

    array1

--- javabase/src/main/java/priv/lyb/study/designpattern/creational/prototype/B.java
+++ javabase/src/main/java/priv/lyb/study/designpattern/creational/prototype/B.java
@@ -1,41 +1,15 @@
 package priv.lyb.study.designpattern.creational.prototype;
 
+import lombok.Data;
+
 /**
  * @author Liuyingbo 2021/1/2
  */
-public class B implements Cloneable{
-    public String getBb() {
-        return bb;
-    }
-
-    public void setBb(String bb) {
-        this.bb = bb;
-    }
-
-    public int getCc() {
-        return cc;
-    }
-
-    public void setCc(int cc) {
-        this.cc = cc;
-    }
-
+@Data
+public class B {
     private String bb;
 
-    private int cc;
-
-    B(String bb,int cc) {
+    B(String bb) {
         this.bb = bb;
-        this.cc = cc;
-    }
-
-    public B clone(){
-        B b = null;
-        try {
-            b = (B) super.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        return b;
     }
 }
