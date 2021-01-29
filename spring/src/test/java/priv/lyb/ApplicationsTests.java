4d059299d7772756d87741608530e767d88d0dff
LYB <lyb0606@163.com>
2021-01-29 16:15:32 +0800

    fixup! add array

---
+++ spring/src/test/java/priv/lyb/ApplicationsTests.java
@@ -0,0 +1,13 @@
+package priv.lyb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
