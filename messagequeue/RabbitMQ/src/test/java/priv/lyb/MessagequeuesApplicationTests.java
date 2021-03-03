88466b4d863e58f9c406ad96eb26ee3999b59f4b
LYB <lyb0606@163.com>
2021-03-03 17:24:51 +0800

    add rabbit，add redis

---
+++ messagequeue/RabbitMQ/src/test/java/priv/lyb/MessagequeuesApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.lyb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagequeuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mysql/mysql.md
@@ -0,0 +1,7 @@
+#MYSQL
+
+##整体执行过程
+![img.png](img.png)
+
+##SQL执行过程
+![img_1.png](img_1.png)
