a001fed9fe33fde19dc6687388a47beb552e7849
LYB <lyb0606@163.com>
2021-03-04 13:41:24 +0800

    add springcloudgateway1.0

--- spring/springcloud/springbootgateway/src/main/java/priv/lyb/GatewayApplication.java
+++ spring/springcloud/springbootgateway/src/main/java/priv/lyb/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZuulApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
