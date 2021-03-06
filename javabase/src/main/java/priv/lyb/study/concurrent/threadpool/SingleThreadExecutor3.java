b7535bdde715e8c9d5cae3dedcd100c5bb809642
LYB <lyb0606@163.com>
2021-03-06 22:07:38 +0800

    add 分布式

--- javabase/src/main/java/priv/lyb/study/concurrent/threadpool/SingleThreadExecutor3.java
+++ javabase/src/main/java/priv/lyb/study/concurrent/threadpool/SingleThreadExecutor3.java
@@ -8,13 +8,13 @@ import java.util.stream.IntStream;
  * @author LiuYingBo 2021/01/30 22:04
  */
 public class SingleThreadExecutor3 {
+
     public static void main(String [] args) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         IntStream.range(0,100)
                 .mapToObj(InterfacingTask::new)
                 .forEach(executorService::execute);
         executorService.shutdown();
-
 
 
 
